7e033a68446e97401cb29aaa45f38891760c7365
alimukhtor <james1999007@gmail.com>
2022-02-03 14:24:04 +0100

    added basic setup

---
+++ strivestaurant/src/components/Home.tsx
@@ -0,0 +1,18 @@
+import * as dataArray from '../data/menu.json'
+const Home =()=> {
+    console.log("Data", dataArray);
+    
+    return(
+        <>
+        {/* {
+            dataArray.map(food=> (
+                <h1>{food.name}</h1>
+
+            )
+
+    )
+        } */}
+        </>
+    )
+}
+export default Home
